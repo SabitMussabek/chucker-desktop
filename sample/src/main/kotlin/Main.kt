9691b65475d09567f9d81a7c6a172383fa266eea
Sabit <sabit.mussabek@gmail.com>
2023-12-06 23:18:35 +0600

    task: add sample do http activity

--- sample/src/main/kotlin/Main.kt
+++ sample/src/main/kotlin/Main.kt
@@ -1,11 +1,33 @@
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
 fun main() = application {
-    Window(
-        title = "Chucker desktop sample",
-        onCloseRequest = ::exitApplication
+    MaterialTheme(
+        colors = lightColors(background = Color(0xFF2b2d30))
     ) {
-
+        Window(
+            title = "Chucker desktop sample",
+            onCloseRequest = ::exitApplication
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(MaterialTheme.colors.background)
+            ) {
+                Button(onClick = {}) {
+                    Text("DO HTTP ACTIVITY")
+                }
+            }
+        }
     }
 }
