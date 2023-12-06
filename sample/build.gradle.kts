b819ee1fe7193821e1984c5c0406b2deb0b82bd1
Sabit <sabit.mussabek@gmail.com>
2023-12-06 23:10:04 +0600

    task: add sample deps

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -1,15 +1,24 @@
 plugins {
     kotlin("jvm") version "1.9.21"
+    id("org.jetbrains.compose") version "1.5.11"
+    id("io.ktor.plugin") version "2.3.6"
 }
 
 group = "com.chuckerdesktop"
-version = "unspecified"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation(project(":library"))
+    implementation(compose.ui)
+    implementation(compose.material)
+    implementation(compose.foundation)
+    implementation(compose.desktop.currentOs)
+    implementation("io.ktor:ktor-client-core")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
 }
 
