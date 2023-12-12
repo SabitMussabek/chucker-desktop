e4c96c1b8f05d89c20dc336f1b0682249617c28a
Sabit <sabit.mussabek@gmail.com>
2023-12-12 17:57:44 +0600

    task: test dependabot 4

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,24 +6,9 @@ include("library")
 include("sample")
 
 dependencyResolutionManagement {
-
     versionCatalogs {
-
-
-        create("libs") {
-
-            version("ktor", "2.3.5")
-            version("kotlinx", "1.7.1")
-            version("compose", "1.5.11")
-            version("kotlin", "1.9.21")
-
-            plugin("jvm", "org.jetbrains.kotlin.jvm").versionRef("kotlin")
-            plugin("compose", "org.jetbrains.compose").versionRef("compose")
-            plugin("ktor", "io.ktor.plugin").versionRef("ktor")
-
-            library("kotlinx-coroutines-core", "org.jetbrains.kotlinx", "kotlinx-coroutines-core").versionRef("kotlinx")
-            library("ktor-client-core", "io.ktor", "ktor").versionRef("ktor")
-
-        }
+//        create("libs") {
+//            from(files("gradle/libs.versions.toml"))
+//        }
     }
 }
