5321a1f20a64860b2b7ac6d5eb03b033c5dec164
Sabit <sabit.mussabek@gmail.com>
2023-12-15 00:59:54 +0600

    task: add HttpClient

---
+++ sample/src/main/kotlin/data/HttpBinHttpTask.kt
@@ -0,0 +1,20 @@
+package data
+
+import com.chuckerdesktop.api.ChuckerDesktopPlugin
+import io.ktor.client.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
+
+
+val client = HttpClient {
+    install(ChuckerDesktopPlugin)
+    install(ContentNegotiation){
+        install(ContentNegotiation) {
+            json(Json {
+                prettyPrint = true
+                isLenient = true
+            })
+        }
+    }
+}
